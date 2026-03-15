a47303e2e6846b88e36cfb055b25de8d933df89f
Pipo-ops <schusterphillip63@gmail.com>
2026-03-15 22:45:51 +0100

    feat:update db.sq

--- sales/models.py
+++ sales/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    newsletter = models.BooleanField(default=False)
+    email_address = models.EmailField()
+    account = models.CharField()

a16ad9b090f32665eb8bc877d5cbfc2b10dc37dd
Pipo-ops <schusterphillip63@gmail.com>
2026-03-17 20:07:29 +0100

    feat: Update the Models.py

--- sales/models.py
+++ sales/models.py
@@ -6,5 +6,5 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     newsletter = models.BooleanField(default=False)
-    email_address = models.EmailField()
-    account = models.CharField()
+    email_address = models.CharField(max_length=100, blank=True, default="")
+    account = models.FloatField(blank=True, null=True)

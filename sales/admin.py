230847e3b76f1dd636d53be2f9b098ac9cef46d8
Pipo-ops <schusterphillip63@gmail.com>
2026-03-19 18:45:06 +0100

    feat: add slug in models for adminpanel and working on the panel first time

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Customer, Product, Bill, Order, Producttype
 
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_filter = ['first_name', 'last_name']
+
+    prepopulated_fields = {"slug": ("first_name", "last_name")}
+
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": ["first_name", "last_name", "email_address", "account"],
+            },
+        ),
+        (
+            "Advanced options",
+            {
+                "classes": ["collapse"],
+                "fields": ["newsletter", "slug"],
+            },
+        ),
+    ]
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product)
+admin.site.register(Bill)
+admin.site.register(Order)
+admin.site.register(Producttype)
--- sales/models.py
+++ sales/models.py
@@ -3,15 +3,27 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30, help_text="max 30 letters dummy")
+    last_name = models.CharField(max_length=30, help_text="max 30 letters dummy")
     newsletter = models.BooleanField(default=False)
     email_address = models.CharField(max_length=100, blank=True, default="")
     account = models.FloatField(blank=True, null=True)
+    slug = models.SlugField(blank=True, default="")
+
+    class Meta:
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
+        ordering = ['first_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
 
 class Bill(models.Model):
     total_amount = models.FloatField()

ea8d302b472827f0a39b31cf042f75d89f046318
Pipo-ops <schusterphillip63@gmail.com>
2026-03-19 15:28:50 +0100

    fix: fix migrate and db

--- sales/models.py
+++ sales/models.py
@@ -9,10 +9,20 @@ class Customer(models.Model):
     email_address = models.CharField(max_length=100, blank=True, default="")
     account = models.FloatField(blank=True, null=True)
 
-class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.FloatField()
 
+class Bill(models.Model):
+    total_amount = models.FloatField()
+    is_paid = models.BooleanField(default=False)    
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='Producttype')
+    bill = models.OneToOneField(Bill, on_delete=models.CASCADE)
+
+class Producttype(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    type_name = models.CharField(max_length=300)
